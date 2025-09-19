02da1d88b45c9dffe3bf46c52fdd846c8e505711
Jordan-Labell <gumblechunkys@gmail.com>
2025-09-19 16:10:24 -0600

    removed extra title on week-2

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
         <main>
-            <h1 className="font-bold text-4xl">Student Information</h1>
             <p>Name: Jordan Labell</p>
             <Link href="https://github.com/Jordan-Labell/cprg306-assignments">Link to Github repository</Link>
         </main>
