05ad6700366014918b2b8194005a0487f2bfa132
Jordan-Labell <gumblechunkys@gmail.com>
2025-09-26 15:43:44 -0600

    updated formatting for item component

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,10 @@
 export default function Item({item}) {
     return (
         <section className="bg-slate-400 p-4 w-1/3 m-4">
-            <h2>Name: {item.name}</h2>
-            <p>Quantity: {item.quantity}</p>
-            <p>Category: {item.category}</p>
+            <ul>
+                <li>Name: {item.name}</li>
+                <li>Buy {item.quantity} in {item.category}</li>
+            </ul>
         </section>
     );
 }
