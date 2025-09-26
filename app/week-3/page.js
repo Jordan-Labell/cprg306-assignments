0050bda44a2147508c6db19f2f1dcc82ceab2480
Jordan-Labell <gumblechunkys@gmail.com>
2025-09-26 16:38:06 -0600

    made list child

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from './item-list';
 export default function Page() {    
     return (
         <main>
-            <div className='flex flex-col justify-center items-center'>
+            <div className='parent-container flex flex-col justify-center items-center'>
                 <h1 className='text-4xl font-extrabold '>Shopping List</h1>
                 <ItemList />
             </div>
