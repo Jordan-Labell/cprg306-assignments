b8d1aef8849ca853660180a2ac5edda46a4d9336
Jordan-Labell <gumblechunkys@gmail.com>
2025-09-26 15:53:14 -0600

    added item list

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,15 +1,10 @@
-import Item from './item';
+import ItemList from './item-list';
 
 export default function Page() {    
-    const item1 = { 
-      name: "Soup",
-      quantity: 3,
-      category: "Golden Retriever",
-    };
     return (
         <main>
             <h1>Shopping List</h1>
-            <Item item={item1} />
+            <ItemList />
         </main>
     );
 }
