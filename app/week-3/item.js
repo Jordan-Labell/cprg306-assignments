8f756e718554019e42a562449b2f3327b2e5b2b1
Jordan-Labell <gumblechunkys@gmail.com>
2025-09-26 16:56:03 -0600

    changed item size

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,6 @@
 export default function Item({item}) {
     return (
-        <section className="bg-slate-400 p-4 m-4">
+        <section className="bg-slate-400 p-4 w-full m-4">
             <ul>
                 <li>Name: {item.name}</li>
                 <li>Buy {item.quantity} in {item.category}</li>
