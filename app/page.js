a8baa1cf628d4a706d09b6b7afe3f47579088d99
Jordan-Labell <gumblechunkys@gmail.com>
2025-09-26 15:38:14 -0600

    converted main page links into a list

--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ export default function Page() {
   return (
     <main>
       <h1 className="font-bold text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2 Assignment</Link>
-      <Link href="/week-3">Week 3 Assignment</Link>
+      <ul>
+        <li><Link href="/week-2">Week 2 Assignment</Link></li>
+        <li><Link href="/week-3">Week 3 Assignment</Link></li>
+      </ul>
     </main>
   );
 }
