75d5683cc2faed1096c9dd72f399c2ffb77a8331
Jordan-Labell <gumblechunkys@gmail.com>
2025-10-10 03:10:17 -0600

    added week 5 page and newItem form component

---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <main className="bg-black h-dvh">
+            <div className="flex flex-col justify-center items-center">
+                <NewItem />
+            </div>
+        </main>
+    );
+}
