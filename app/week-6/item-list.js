386bced58bcd7cd07e507027d5656978c9d06e35
jordan.labell1 <jordan.labell1@ucalgary.ca>
2025-10-13 23:40:34 -0600

    added buttons for sorting and improved look

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -4,10 +4,10 @@ import Item from './item';
 import itemsList from './items.json';
 
 export default function ItemList() { 
-    const [sortBy, setSortBy] = useState("name");
+    const [sortBy, setSortBy] = useState("category");
     let itemsListCopy = [...itemsList];
 
-    itemsListCopy.sort((item1, item2) => {
+      itemsListCopy.sort((item1, item2) => {
       if (sortBy === "name") {
         if (item1.name < item2.name) return -1;
         if (item1.name > item2.name) return 1;
@@ -24,9 +24,20 @@ return (
         <ul className='mx-auto max-w-md'>
           <li>
             <div className='flex my-2 gap-1'>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sort by Name</button>
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sort by Category</button>
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sort by Category</button>
+              <button 
+                onClick={() => setSortBy("name")} 
+                className={`text-white font-bold py-2 px-4 rounded cursor-pointer ${sortBy == "name" ? 'bg-blue-700' : 'bg-slate-500'}`}>
+                Sort by Name
+              </button>
+              <button 
+                onClick={() => setSortBy("category")} 
+                className={`text-white font-bold py-2 px-4 rounded cursor-pointer ${sortBy == "category" ? 'bg-blue-700' : 'bg-slate-500'}`}>
+                Sort by Category
+              </button>
+              <button 
+                className={`text-white font-bold py-2 px-4 rounded cursor-pointer ${sortBy == "groupCategory" ? 'bg-blue-700' : 'bg-slate-500'}`}>
+                Group by Category
+              </button>
             </div>
           </li>
           <li>
